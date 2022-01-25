2ef51cdbbcbcfe17fbe5651371e007e102957716
Naitik Shah <n@daaku.org>
2022-01-25 20:59:33 +0400

    80col

--- src/daaku/sqjson.clj
+++ src/daaku/sqjson.clj
@@ -22,25 +22,34 @@
                            PersistentHashSet {:tag "!set"
                                               :encode jt/encode-collection
                                               :decode set}
-                           java.time.OffsetDateTime {:tag "!odt"
-                                                     :encode jt/encode-str
-                                                     :decode #(java.time.OffsetDateTime/parse %)}
-                           java.time.LocalDateTime {:tag "!ldt"
-                                                    :encode jt/encode-str
-                                                    :decode #(java.time.LocalDateTime/parse %)}}})]}))
+                           java.time.OffsetDateTime
+                           {:tag "!odt"
+                            :encode jt/encode-str
+                            :decode #(java.time.OffsetDateTime/parse %)}
+                           java.time.LocalDateTime
+                           {:tag "!ldt"
+                            :encode jt/encode-str
+                            :decode #(java.time.LocalDateTime/parse %)}}})]}))
 
 (defn make-options [{:keys [mapper table]
                      :or {mapper mapper table "doc"}}]
   {:migrations [(str "create table if not exists " table "(data text)")
-                (str "create unique index if not exists " table "_id on " table "(json_extract(data, '$.id'))")]
+                (str "create unique index if not exists "
+                     table "_id on " table "(json_extract(data, '$.id'))")]
    :insert (str "insert into " table "(data) values(?) returning data")
    :select (str "select data from " table " where ")
-   :delete-one (str "delete from " table " where rowid in (select rowid from " table " where ")
+   :delete-one (str "delete from " table " where rowid in (select rowid from "
+                    table " where ")
    :delete-all (str "delete from " table " where ")
-   :patch-one (str "update " table " set data = json_patch(data, ?) where rowid in (select rowid from " table " where ")
+   :patch-one (str "update " table " set data = json_patch(data, ?) "
+                   "where rowid in (select rowid from " table " where ")
    :patch-all (str "update " table " set data = json_patch(data, ?) where ")
-   :replace-one (str "update " table " set data = ? where rowid in (select rowid from " table " where ")
-   :upsert (str "insert into " table "(data) values(?) on conflict(json_extract(data, '$.id')) do update set data = excluded.data returning data")
+   :replace-one (str "update " table
+                     " set data = ? where rowid in (select rowid from "
+                     table " where ")
+   :upsert (str "insert into " table "(data) values(?) "
+                "on conflict(json_extract(data, '$.id')) do update "
+                "set data = excluded.data returning data")
    :count (str "select count(*) as count from " table " where ")
    :mapper mapper})
 
@@ -109,7 +118,8 @@
 
         (contains? #{:in :not-in} op)
         (let [[p v] va]
-          [(str (encode-path p) (op-str op) "(" (str/join "," (repeat (clojure.core/count v) "?")) ")")
+          [(str (encode-path p) (op-str op) "("
+                (str/join "," (repeat (clojure.core/count v) "?")) ")")
            (map encode-sql-param v)])
 
         :else
@@ -140,11 +150,13 @@
 
 (defmacro ^:private one-where [ds sql-key sql-suffix where params-prefix]
   `(let [[sql# params#] (encode-where ~where)]
-     (one-doc ~ds (concat [(str (~sql-key *opts*) sql# ~sql-suffix)] ~params-prefix params#))))
+     (one-doc ~ds (concat [(str (~sql-key *opts*) sql# ~sql-suffix)]
+                          ~params-prefix params#))))
 
 (defmacro ^:private one-val [ds sql-key where params-prefix kw]
   `(let [[sql# params#] (encode-where ~where)]
-     (-> (jdbc/execute-one! ~ds (concat [(str (~sql-key *opts*) sql#)] ~params-prefix params#))
+     (-> (jdbc/execute-one! ~ds (concat [(str (~sql-key *opts*) sql#)]
+                                        ~params-prefix params#))
          ~kw)))
 
 (defn insert [ds doc]
@@ -160,13 +172,15 @@
   (one-val ds :delete-all where nil :next.jdbc/update-count))
 
 (defn patch [ds where patch]
-  (one-where ds :patch-one " limit 1) returning data" where [(encode-doc patch)]))
+  (one-where ds :patch-one " limit 1) returning data" where
+             [(encode-doc patch)]))
 
 (defn patch-all [ds where patch]
   (one-val ds :patch-all where [(encode-doc patch)] :next.jdbc/update-count))
 
 (defn replace [ds where doc]
-  (one-where ds :replace-one " limit 1) returning data" where [(encode-doc doc)]))
+  (one-where ds :replace-one " limit 1) returning data" where
+             [(encode-doc doc)]))
 
 (defn upsert [ds doc]
   (one-doc ds [(:upsert *opts*) (-> doc add-id encode-doc)]))
@@ -177,4 +191,5 @@
 (defn select [ds where]
   (let [[sql params] (encode-where where)]
     (map (comp decode-doc first)
-         (rest (jdbc/execute! ds (concat [(str (:select *opts*) sql)] params) jdbc-opts)))))
+         (rest (jdbc/execute! ds (concat [(str (:select *opts*) sql)] params)
+                              jdbc-opts)))))
